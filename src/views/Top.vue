83c8042be28ddb2e38585c084f8c38625d421382
Deon Blaauw <blaauw.deon@gmail.com>
2023-05-31 14:07:20 +0200

    added toggle between multiple chart types

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,56 +1,98 @@
 <template>
   <div>
     <h2>Firebase vue Startup kit</h2>
+    <label>
+      <input type="radio" value="bar" v-model="chartType"> Bar Chart
+    </label>
+    <label>
+      <input type="radio" value="pie" v-model="chartType"> Pie Chart
+    </label>
+    <label>
+      <input type="radio" value="line" v-model="chartType"> Line Chart
+    </label>
     <div class="chart-container">
-      <div id="myDiv"></div> <!-- Bar chart will be drawn inside this div -->
-      <div id="myDivPie"></div> <!-- Pie chart will be drawn inside this div -->
+      <div id="myDiv" v-show="chartType === 'bar'"></div>
+      <div id="myDivPie" v-show="chartType === 'pie'"></div>
+      <div id="myDivLine" v-show="chartType === 'line'"></div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.chart-container {
-  display: flex;
-  justify-content: space-around; /* Optional. This adds some space between the charts */
-}
-</style>
 
 <script>
 import Plotly from 'plotly.js-dist';
 
 export default {
   name: 'Top',
-  mounted() {
-    // Bar chart data and layout
-    let barData = [{
-      x: ['Category 1', 'Category 2', 'Category 3'], // Replace with your actual data
-      y: [10, 15, 7], // Replace with your actual data
-      type: 'bar'
-    }];
-
-    let barLayout = {
-      title: 'Bar Chart',
-      width: 500,
-      height: 500
-    };
-
-    Plotly.newPlot('myDiv', barData, barLayout);
-    
-    // Pie chart data and layout
-    let pieData = [{
-      values: [19, 26, 55], // Replace with your actual data
-      labels: ['Residential', 'Non-Residential', 'Utility'], // Replace with your actual data
-      type: 'pie'
-    }];
-
-    let pieLayout = {
-      title: 'Pie Chart',
-      height: 400,
-      width: 500
-    };
-
-    Plotly.newPlot('myDivPie', pieData, pieLayout);
+  data() {
+    return {
+      chartType: 'bar', // Initial chart type
+    }
   },
+  mounted() {
+    this.renderCharts();
+  },
+  watch: {
+    chartType() {
+      this.renderCharts();
+    }
+  },
+  methods: {
+    renderCharts() {
+      if(this.chartType === 'bar') {
+        let barData = [{
+          x: ['Category 1', 'Category 2', 'Category 3'],
+          y: [10, 15, 7],
+          type: 'bar'
+        }];
+
+        let barLayout = {
+          title: 'Bar Chart',
+          width: 500,
+          height: 500
+        };
+
+        Plotly.newPlot('myDiv', barData, barLayout);
+      }
+      
+      if(this.chartType === 'pie') {
+        let pieData = [{
+          values: [19, 26, 55],
+          labels: ['Residential', 'Non-Residential', 'Utility'],
+          type: 'pie'
+        }];
+
+        let pieLayout = {
+          title: 'Pie Chart',
+          height: 400,
+          width: 500
+        };
+
+        Plotly.newPlot('myDivPie', pieData, pieLayout);
+      }
+
+      if(this.chartType === 'line') {
+        let lineData = [{
+          x: [1, 2, 3, 4],
+          y: [10, 15, 13, 17],
+          mode: 'lines'
+        }];
+
+        let lineLayout = {
+          title: 'Line Chart',
+          height: 400,
+          width: 500
+        };
+
+        Plotly.newPlot('myDivLine', lineData, lineLayout);
+      }
+    }
+  }
 };
 </script>
 
+<style scoped>
+.chart-container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
