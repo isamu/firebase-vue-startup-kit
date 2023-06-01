cb3b655f9dcb9b7e2ea9849af1ad852c11b9925b
snakajima <satoshi.nakajima@gmail.com>
2023-06-01 08:25:49 -0700

    openai

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
-
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
+import openai
+import os
+
+# Configuration
+load_dotenv() # Load default environment variables (.env)
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
 
 app = Flask(__name__)
 
