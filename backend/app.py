c10caaac84fa2c8da09fce6aa6b6252a4e3c3ced
snakajima <satoshi.nakajima@gmail.com>
2023-06-01 08:38:52 -0700

    /api/chat

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,10 @@ import os
 # Configuration
 load_dotenv() # Load default environment variables (.env)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+OPENAI_API_MODEL = os.getenv("OPENAI_API_MODEL", "gpt-3.5-turbo")
+OPENAI_TEMPERATURE = float(os.getenv("OPENAI_TEMPERATURE", 0.7))
 assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
+openai.api_key = OPENAI_API_KEY
 
 app = Flask(__name__)
 
@@ -16,9 +19,13 @@ def hello_world():
     name = request.args.get('name')
     return f"<p>Hello, {name or 'World'}!</p>"
 
-@app.route("/api/hello")
+@app.route("/api/chat")
 def hello():
-    return jsonify(message='Hello, world!')
+    messages = []
+    query = request.args.get('query')
+    messages.append({"role":"user", "content":query or "Hello."})
+    response = openai.ChatCompletion.create(model=OPENAI_API_MODEL, messages=messages, temperature=OPENAI_TEMPERATURE)
+    return jsonify(response['choices'][0]['message'])
 
 if (__name__ == '__main__'):
     app.run(port=8081)
