65f1713feaa0c434135f8b04566ac3cd71c8c64b
snakajima <satoshi.nakajima@gmail.com>
2023-05-31 21:39:56 -0700

    api

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/hello")
+def hello():
+    return jsonify(message='Hello, world!')
 
